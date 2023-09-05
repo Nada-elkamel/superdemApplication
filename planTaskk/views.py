408a8d16ab1127d9f288c37b57a18514e724bd7f
Nada El Kamel <114959945+Nada-elkamel@users.noreply.github.com>
2023-09-05 23:18:32 +0100

    Add files via upload

--- planTaskk/views.py
+++ planTaskk/views.py
@@ -16,12 +16,16 @@ from django.core.paginator import Paginator
 from .models import UserProfile
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
+from urllib.parse import parse_qs
+
 
 current_datetime = timezone.now()
 emp = User.objects.filter(is_active=True)
 employes = [employe.username for employe in emp]
 sources = {'Mail','Appel reçu','Interne'}
 natures= {'Commercial','Logistique','Point interne','Facture Client'}
+
+# Cette fonction permet a l'utilisateur de s'authentifier
 def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -42,30 +46,43 @@ def login_view(request):
     return render(request, 'login.html')
 
 
+#cette fonction permet a l'utilisateur de se déconnecter
 @login_required(login_url='login')
 def signOut(request):
     logout(request) 
     return HttpResponseRedirect(reverse('login_view'))
 
+#fonction qui permet de lister les taches
 @login_required(login_url='login')
 def TaskList(request):
     user_profile = UserProfile.objects.get(user=request.user)
+    
+    # Récupérez toutes les tâches
     toDo = Todo.objects.all()
     
-    paginator = Paginator(toDo, 4)  # 4  tasks per page
+    # Récupérez le filtre de l'URL
+    filter_option = request.GET.get('filter')
+    
+    # Si l'option de filtre est "tasks_for_me", filtrez les tâches pour l'utilisateur connecté
+    if filter_option == 'tasks_for_me':
+        toDo = toDo.filter(responsable=request.user)
+    
+    paginator = Paginator(toDo, 4)  # 4 tâches par page
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     
     context = {
         'user_profile': user_profile,
         'page_obj': page_obj,
-        'employes' : employes,
-        'current_page' : 1
+        'employes' : employes,  # Assurez-vous que cette variable est correctement définie
+        'current_page' : 1,
+        'filter_option': filter_option  # Ajoutez l'option de filtre au contexte
     }
     
     return render(request, 'TaskList.html', context)
 
 
+#fonction qui affiche le tableau de bord 
 @login_required(login_url='login')
 def dashboard (request):
     user_profile = UserProfile.objects.get(user=request.user)
@@ -133,6 +150,7 @@ def dashboard (request):
         }
     return HttpResponse(template.render(context, request))
 
+#cette fonction affiche le profil de l'utilisateur
 @login_required(login_url='login')
 def profile (request):
     user_profile = UserProfile.objects.get(user=request.user)
@@ -142,33 +160,7 @@ def profile (request):
     }
     return HttpResponse (template.render(context,request))
 
-@login_required(login_url='login')
-def add_todo(request):
-    if request.method == 'POST':
-        status = request.POST.get('status')
-        current_datetime = timezone.now()
-        createur = request.user.username
-        responsable = request.POST.get('responsable')
-        source = request.POST.get('source')
-        nature = request.POST.get('nature')
-        deadline = request.POST.get('deadline') 
-        tache = request.POST.get('tache')
-        todo = Todo(
-            datecreation=current_datetime,
-            status=status,
-            createur = createur,
-            responsable = responsable, 
-            source = source,
-            nature = nature,
-            tache = tache,
-            deadline = deadline
-        )
-
-        todo.save()
-        messages.success(request, "La tache a été ajoutée avec succès")
-
-        return redirect('TaskList')
-
+#fonction pour modifier le profil
 @login_required(login_url='login')
 def edit_profile(request):
     user = request.user
@@ -211,7 +203,35 @@ def edit_profile(request):
     }
     return render(request, 'profile.html', context)
 
-from urllib.parse import parse_qs
+#fonction d'ajout d'une tache
+@login_required(login_url='login')
+def add_todo(request):
+    if request.method == 'POST':
+        status = request.POST.get('status')
+        current_datetime = timezone.now()
+        createur = request.user.username
+        responsable = request.POST.get('responsable')
+        source = request.POST.get('source')
+        nature = request.POST.get('nature')
+        deadline = request.POST.get('deadline') 
+        tache = request.POST.get('tache')
+        todo = Todo(
+            datecreation=current_datetime,
+            status=status,
+            createur = createur,
+            responsable = responsable, 
+            source = source,
+            nature = nature,
+            tache = tache,
+            deadline = deadline
+        )
+
+        todo.save()
+        messages.success(request, "La tâche a été ajoutée avec succès")
+
+        return redirect('TaskList')
+
+#fonction qui affiche la page de l'edition
 @login_required(login_url='login')
 def edit(request,task_id,current_page):
     user_profile = UserProfile.objects.get(user=request.user)
@@ -228,7 +248,8 @@ def edit(request,task_id,current_page):
     }
     return render(request, 'edit.html',context)
 
-
+#fonction qui permet de modifier une tache
+@login_required(login_url='login')
 def update_task(request, task_id):
     task = Todo.objects.get( id=task_id)
     if request.method == 'POST':
@@ -241,24 +262,32 @@ def update_task(request, task_id):
         if task.status == 'Interne':
             task.interne = request.POST['interne']
         task.save()
+        messages.success(request, "La tâche a été modifiée avec succès")
+
         current_page = request.POST['current_page']
-        return HttpResponseRedirect('/planTaskk/TaskList?page='+ str(current_page))  # Redirect back to the task list
+        return HttpResponseRedirect('/planTaskk/TaskList?page='+ str(current_page)) 
     return render(request, 'edit_task.html', {'task': task})
 
-
+#fonction qui permet de marquer une tache en cours
+@login_required(login_url='login')
 def start_task(request,task_id,current_page):
     task = Todo.objects.get( id=task_id)
     task.status = "En cours"
     task.save()
+    messages.success(request, "La tâche est en cours d'exécution")
+
     
-    return HttpResponseRedirect('/planTaskk/TaskList?page='+ str(current_page))  # Redirect back to the task list
+    return HttpResponseRedirect('/planTaskk/TaskList?page='+ str(current_page))  # redirige vers la page actuelle 
 
-
+#fonction qui permet de marquer une tache comme terminée
+@login_required(login_url='login')
 def task_done(request):
     task_id = request.POST['task_id']
     task = Todo.objects.get( id=task_id)
     task.status = "Completé"
     task.commentaire = request.POST['commentaire']
     task.save()
+    messages.success(request, "La tâche terminée  ")
+
     return redirect('TaskList')
 
